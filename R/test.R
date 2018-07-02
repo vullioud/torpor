1eb6e8b6d581e5dd2fa3716dc1f3259ef45f29dd
vullioud <colino.vullioud@gmail.com>
2018-07-02 13:47:06 +0200

    add model into inst/exdsata/

--- R/test.R
+++ R/test.R
@@ -1,1 +1,12 @@
-## test
+#' Fit models
+#'
+#'wrapper around the Jags function -
+#'matic rounding
+#'@name fit_model
+#'@param data model to be used
+#'@param BMR name of the model
+#'@param TLC
+#'@return a fitted jags object
+#'@import rjags
+#'@import dplyr
+#'@export
