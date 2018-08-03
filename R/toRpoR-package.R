cef718a11d749b02cf9e169c662f2cb49e951a82
vullioud <colino.vullioud@gmail.com>
2018-08-03 12:33:31 +0200

    add the general help

--- R/toRpoR-package.R
+++ R/toRpoR-package.R
@@ -1,18 +1,29 @@
-#' toRpoR package
+#' toRpoR: a package to fit mixture models on bats
 #'
-#' This R package gives the tools to reproduce the main statistical analysis of
+#' This R package is center around 3 main functions:
+#' the fit_torpor() functions allow you to fit gives the tools to reproduce the main statistical analysis of
 #' the article: Super Super
 #'
-#' This package has not been conceived for general use yet!
+#' This package is not conceived for general use yet!
 #'
 #' In the examples below, we provide the workflow leading the main results
 #' presented in the paper.
 #'
 #'
 #' NOTE: IT is a mess
-#'
-#' @name toRpoR-package
-#' @aliases toRpoR-package
 #' @docType package
-#' @keywords package
-#' @export
+#' @name toRpoR
+#'
+#' @section fit_torpor():
+#' The fit_torpor() function fit a mixture model for 2 pop using jags.
+#'
+#' @section fit_and_plot():
+#' The fit_and_plot() isa wrapper function around the fit_torpor() function. It
+#' reproduce the plot presented in the article.
+#' @section get_prediction():
+#' The get_prediction() functions allow you to access the prediction of the
+#' fitted model and the 95 credibility intervals.
+#' @section get_Q10():
+#' The get_Q10() function gives you the Q10 values for a given dataset.
+#'
+NULL
