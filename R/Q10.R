bc21c51e62e714080a6b737d95bf5bd05383e69e
vullioud <colino.vullioud@gmail.com>
2018-11-08 16:53:03 +0000

    changed arg name

--- R/Q10.R
+++ R/Q10.R
@@ -9,21 +9,21 @@
 #'@export
 get_Q10 <- function(mod){
 
-  beta1<- stats::median(model$sims.list$beta1)
-  beta2 <- stats::median(model$sims.list$beta2)
-  int1 <- stats::median(model$sims.list$int1)
-  int2 <- stats::median(model$sims.list$int2)
-  int3 <- stats::median(model$sims.list$int3)
-  Tmin <- stats::median(model$sims.list$Tmin)
-  TLC <- 	stats::median(model$sims.list$tlc)
+  beta1<- stats::median(mod$sims.list$beta1)
+  beta2 <- stats::median(mod$sims.list$beta2)
+  int1 <- stats::median(mod$sims.list$int1)
+  int2 <- stats::median(mod$sims.list$int2)
+  int3 <- stats::median(mod$sims.list$int3)
+  Tmin <- stats::median(mod$sims.list$Tmin)
+  TLC <- 	stats::median(mod$sims.list$tlc)
 
   out <- round((funtorp(TLC, Tmin, int2, int3, beta1, beta2)/
                   funtorp(Tmin, Tmin, int2, int3, beta1, beta2))
                ^(10/(TLC-Tmin)),digits=1)
 
-  if(length(model$mean$G[model$mean$G>1.5]) == 0){
+  if(length(mod$mean$G[mod$mean$G>1.5]) == 0){
     stop("no values in torpor")
-  } else if (length(model$mean$G[model$mean$G<1.5]) == 0){
+  } else if (length(mod$mean$G[mod$mean$G<1.5]) == 0){
     out <- out
   } else {
     out <- out
