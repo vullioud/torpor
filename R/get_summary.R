057e2149e03d778a0d57d19ee318e40e65d04a6b
vullioud <colino.vullioud@gmail.com>
2019-03-28 18:13:37 +0100

    summary function first commit

--- R/get_summary.R
+++ R/get_summary.R
@@ -1,48 +1,47 @@
-# library(truncnorm)
-#
-#
-# get_summary <- function(mod){
-# out <- list()
-#
-# params <- c("tauy", "inte", "intc", "intr", "betat", "betac", "Tt", "tlc")
-#
-# mean <- unlist(test$mean[params])
-# CI_97.5 <- unlist(test$q97.5[params])
-# CI_2.5 <- unlist(test$q2.5[params])
-# Rhat <- unlist(test$Rhat[params])
-#
-# ## frame the output in a df
-# x <- as.data.frame(cbind(mean, CI_2.5, CI_97.5, Rhat))
-# x$parameter <- rownames(x)
-# x <- x[,c(5,1,2,3,4)]
-# rownames(x) <- NULL
-#
-# out$parameter_estimates <- x
-# ############### OVERLAP
-# prior_tlc <- rtruncnorm(20000,a= test$mean$TLC,mean=0,sd=100)
-# prior_Tt <- rtruncnorm(20000,b=test$mean$TLC,mean=0,sd=100)
-# prior_betat <- rtruncnorm(20000,b=0,mean=0,sd=100)
-#
-#
-# tlc_overlap <- check_overlap(test, "tlc", prior_tlc)
-# Tt_overlap <- check_overlap(test, "Tt", prior_Tt)
-# betat_overlap <- check_overlap(test, "betat", prior_betat)
-#
-# if(Tt_overlap >= 0.3){
-#   warning("Parameters Tt and intr not identifiable")
-# }
-# if(tlc_overlap >= 0.3){
-#   warning("Parameters tlc, betac, and intc not identifiable")
-# }
-# if(betat_overlap >= 0.3){
-#   warning("Parameters tlc, betac, and intc not identifiable")
-# }
-# out$overlap <- list(tlc = tlc_overlap,
-#                           Tt = Tt_overlap,
-#                           Betat =betat_overlap)
-#
-# ############## Q10
-# out$Q10 <- get_Q102(test)
-# test
-#
-# }
+#' get_summary
+#'
+#' This function return a comprehensive summary of the model fit. It returns the
+#' estimated parameters of the model. The check of overlap, Q10,
+#'@name get_summary
+#'@aliases get_summary
+#'@param mod a fitted model with fit_torpor
+#'@return a list
+#'@import purrr
+#'@import overlapping
+#'@import dplyr
+#'@export
+#'@examples
+#'data(test_data2)
+#'test2 <- fit_torpor2(MR = test_data2[,2],
+#'Ta = test_data2[, 1],
+#'BMR = 1.49,
+#'TLC = 28.8,
+#'Model = NULL,
+#'fitting_options = list(nc = 2))
+#'get_summary(mod = test2)
+
+get_summary <- function(mod){
+out <- list()
+
+params <- c("tauy", "inte", "intc", "intr", "betat", "betac", "Tt", "tlc")
+
+mean <- unlist(mod$mean[params])
+CI_97.5 <- unlist(mod$q97.5[params])
+median <- unlist(mod$q50[params])
+CI_2.5 <- unlist(mod$q2.5[params])
+Rhat <- unlist(mod$Rhat[params])
+
+## frame the output in a df
+x <- as.data.frame(cbind(mean, CI_2.5, median, CI_97.5, Rhat))
+x$parameter <- rownames(x)
+x <- x[,c(6,1,2,3,4, 5)]
+rownames(x) <- NULL
+
+out$parameter_estimates <- x
+############### OVERLAP
+out$overlap <- check_overlap(mod)
+out$Ym <- mod$mean$Ym
+##############
+return(out)
+
+}
