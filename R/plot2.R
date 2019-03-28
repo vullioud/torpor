aa4ff3f44df6f45ad321fb51192f8276b72a2cfe
vullioud <colino.vullioud@gmail.com>
2019-03-28 18:15:22 +0100

    removed MR and Ta arg. add col arg. simplified code. add warnings for overlap

--- R/plot2.R
+++ R/plot2.R
@@ -5,11 +5,11 @@
 #'stage and regression slopes (continuous lines) as well as 95% CI (segmented lines) are presented.
 #'For more flexibility the users are advised to use [fit_torpor()] and [get_prediction()] directly.
 #'@name fit_and_plot2
-#'@param MR a vector with MR
-#'@param Ta, a vector with Ta (same length as MR)
 #'@param mod a fitted model from fit torpor
-#'@param ... arguments to fit the model in [fit_torpor()] if no model output are provided
 #'@param plot_type character string specifying the type of plot desired. either "base" or "ggplot"
+#'@param ... arguments to fit the model in [fit_torpor()] if no model output are provided
+#'@param col_torp plot col for torpor
+#'@param col_eut plot col for euthermie
 #'@export
 #'@return a base-R plot or a ggplot object
 #'@examples
@@ -19,21 +19,17 @@
 #'plot2 <- fit_and_plot2(MR = test_data3[,2], Ta = test_data3[,1], BMR = 1.055, TLC = 29,
 #'fitting_options = list(nc =1), plot_type = "ggplot")
 
-fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot", MR, Ta,...) {
+fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", col_eut = "black",...) {
   # browser()
   ## fit a model if necessary
   if(is.null(mod)) {
-    out <- fit_torpor2(MR = MR, Ta = Ta,...)
+    out <- fit_torpor2(...)
   } else {
     out <- mod
   }
 
   TLC <- out$sims.list$TLC[1]
   BMR <- out$sims.list$BMR[1]*out$sims.list$Ym[1]
-  # ## remove NAs / reshufled the data as done for the fit of the model
-  # set.seed(666)
-  # da <- as.data.frame(cbind(MR, Ta)[!is.na(MR) & !is.na(Ta) & Ta < (TLC - 2), ])
-  # da <- da[sample(nrow(da)), ]
 
 
   Y <- MR <- out$data$Y*out$sims.list$Ym[1]
@@ -51,9 +47,13 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot", MR, Ta,...) {
   da$hat <- out$Rhat$G
 
 
-    # get the predictions
+  # get the predictions
   pred <- get_prediction2(out, seq(Tlimlo,Tlimup,length=100))
+  # check overlap
+  xxx <- check_overlap(out)
 
+
+  ###### plot GGplot first
   if(plot_type == "ggplot"){
     G <- lwr_95 <- upr_95 <- NULL
     ggplot2::ggplot(da, ggplot2::aes(x = Ta, y = MR, col = G > 1.5))+
@@ -63,27 +63,28 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot", MR, Ta,...) {
       ggplot2::geom_line(data = pred[pred$group == "Norm", ],
                          ggplot2::aes(x = Ta, y = pred),
                          inherit.aes = F,
-                         col = "red",
+                         col = col_eut,
                          linetype = 2) +
       ggplot2::geom_ribbon(data = pred[pred$group == "Norm", ],
                            ggplot2::aes(x = Ta, ymin = lwr_95, ymax = upr_95),
                            inherit.aes = F,
                            alpha = 0.2,
-                           fill = "red",
+                           fill = col_eut,
                            col = NA)+
       ggplot2::geom_line(data = pred[pred$group == "Torp", ],
                          ggplot2::aes(x = Ta, y = pred),
                          inherit.aes = F,
-                         col = "blue",
+                         col = col_torp,
                          linetype = 2) +
       ggplot2::geom_ribbon(data = pred[pred$group == "Torp", ],
                            ggplot2::aes(x = Ta, ymin = lwr_95, ymax = upr_95),
                            inherit.aes = F,
                            alpha = 0.2,
-                           fill = "blue",
+                           fill = col_torp,
                            col = NA) +
-      ggplot2::scale_color_discrete("",
-                                    labels = c("Normothermy", "Torpor"))
+      ggplot2::scale_color_manual("",
+                                    labels = c("Euthermy", "Torpor"),
+                                    values =  c(col_eut, col_torp))
 
   } else {
     X <-  pred[pred$group == "Norm", "Ta"]
@@ -93,40 +94,40 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot", MR, Ta,...) {
     Ymeann <- pred[pred$group == "Norm", "pred"]
     Y975n <- pred[pred$group == "Norm", "upr_95"]
     Y025n <- pred[pred$group == "Norm", "lwr_95"]
-
+    ylab <- "MR"
     ## plot
     graphics::plot(Y~Ta,type="n",frame=F, xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),ylab=ylab)
-    graphics::points(Y[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col="red",pch=19)
-    graphics::points(Y[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col="blue",pch=19)
-    graphics::points(Y[out$mean$G>=1.5&out$Rhat$G>=1.1]~Ta[out$mean$G>=1.5&out$Rhat$G>=1.1],col="blue",pch=19)
-    graphics::points(Y[out$mean$G<1.5&out$Rhat$G>=1.1]~Ta[out$mean$G<1.5&out$Rhat$G>=1.1],col="red",pch=19)
+    graphics::points(Y[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col= col_eut,pch=19)
+    graphics::points(Y[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col= col_torp,pch=19)
+    graphics::points(Y[out$mean$G>=1.5&out$Rhat$G>=1.1]~Ta[out$mean$G>=1.5&out$Rhat$G>=1.1],col= col_torp,pch=19)
+    graphics::points(Y[out$mean$G<1.5&out$Rhat$G>=1.1]~Ta[out$mean$G<1.5&out$Rhat$G>=1.1],col= col_eut ,pch=19)
 
 
     if(length(out$mean$G[out$mean$G>1.5&out$Rhat$G<=1.1])>0){
       graphics::par(new=T)
 
-      graphics::plot(Ymeant~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="blue")
+      graphics::plot(Ymeant~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_torp)
       graphics::par(new=T)
 
-      graphics::plot(Y975t~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="blue")
+      graphics::plot(Y975t~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_torp)
       graphics::par(new=T)
 
-      graphics::plot(Y025t~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="blue")
+      graphics::plot(Y025t~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_torp)
     }
 
 
     if(length(out$mean$G[out$mean$G<1.5&out$Rhat$G<=1.1])>0){
       graphics::par(new=T)
 
-      graphics::plot(Ymeann~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="red")
+      graphics::plot(Ymeann~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_eut)
       graphics::par(new=T)
 
-      graphics::plot(Y975n~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="red")
+      graphics::plot(Y975n~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_eut)
       graphics::par(new=T)
 
-      graphics::plot(Y025n~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col="red")}
+      graphics::plot(Y025n~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_eut)}
 
-    graphics::legend("topright",c("Euthermy","Torpor"),pch=19, col=c("red","blue"),bty="n")
+    graphics::legend("topright",c("Euthermy","Torpor"),pch=19, col=c(col_eut,col_torp),bty="n")
   }
 }
 
