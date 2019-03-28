0be4dd1822ca488fde28f45bbfb934d5f896686f
vullioud <colino.vullioud@gmail.com>
2019-03-28 18:12:29 +0100

    removed examples / fixed typos

--- R/Q102.R
+++ R/Q102.R
@@ -2,7 +2,6 @@
 #'
 #'The function provides the Q10 temperature coefficient,
 #'which represents the metabolic rates ratio measured at 10 °C Ta apart.
-
 #'@name get_Q102
 #'@param mod a fitted model from fit_torpor
 #'@return the Q10 value
@@ -22,7 +21,7 @@ get_Q102 <- function(mod){
 
 
   out <- round((funtorp2(tlc, Tt, intr, intc, betat, betac, Ym)/
-    funtorp2(Tt, Tt, intr, intc, betat, betac, Ym))^(10/(tlc-Tt)), digit = 1)
+    funtorp2(Tt, Tt, intr, intc, betat, betac, Ym))^(10/(tlc-Tt)), digits = 1)
 
   if(length(mod$mean$G[mod$mean$G>1.5]) == 0){
     warning("no values in torpor")
@@ -32,3 +31,5 @@ get_Q102 <- function(mod){
   }
   return(out)
 }
+
+
