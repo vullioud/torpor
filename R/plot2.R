71867c210eb377105c19dc3f004eaf46f0311b45
vullioud <colino.vullioud@gmail.com>
2019-03-29 14:25:30 +0100

    updated link to other functions

--- R/plot2.R
+++ R/plot2.R
@@ -3,24 +3,28 @@
 #'The function provides a plot of the MR values over the respective Ta.
 #'Measures are presented in different colors depending of the metabolic
 #'stage and regression slopes (continuous lines) as well as 95% CI (segmented lines) are presented.
-#'For more flexibility the users are advised to use [fit_torpor()] and [get_prediction()] directly.
+#'For more flexibility the users are advised to use [fit_torpor2()] and [get_prediction()] directly.
 #'@name fit_and_plot2
 #'@param mod a fitted model from fit torpor
 #'@param plot_type character string specifying the type of plot desired. either "base" or "ggplot"
-#'@param ... arguments to fit the model in [fit_torpor()] if no model output are provided
+#'@param ... arguments to fit the model in [fit_torpor2()] if no model output are provided
 #'@param col_torp plot col for torpor
 #'@param col_eut plot col for euthermie
+#'@param ylab y label
+#'@param xlab x label
 #'@param pdf
+#'@import ggplot2
 #'@export
 #'@return a base-R plot or a ggplot object
 #'@examples
 #'data(test_data3)
+#'fit_and_plot2(test, col_eut = "red", plot_type = "base", pdf = TRUE)
 #'plot1 <- fit_and_plot2(MR = test_data3[,2], Ta = test_data3[,1], BMR = 1.055, TLC = 29,
 #'fitting_options = list(nc =1), plot_type = "base")
 #'plot2 <- fit_and_plot2(MR = test_data3[,2], Ta = test_data3[,1], BMR = 1.055, TLC = 29,
 #'fitting_options = list(nc =1), plot_type = "ggplot")
 
-fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", col_eut = "black", pdf = FALSE, ...) {
+fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", col_eut = "black", ylab = "MR", xlab = "Ta", pdf = FALSE, ...) {
   # browser()
   ## fit a model if necessary
   if(is.null(mod)) {
@@ -57,7 +61,7 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", co
   ###### plot GGplot first
   if(plot_type == "ggplot"){
     G <- lwr_95 <- upr_95 <- NULL
-    ggplot2::ggplot(da, ggplot2::aes(x = Ta, y = MR, col = G > 1.5))+
+    plot <- ggplot2::ggplot(da, ggplot2::aes(x = Ta, y = MR, col = G > 1.5))+
       ggplot2::geom_point() +
       ggplot2::xlim(c(min(da$Ta), max(da$Ta))) +
       # ylim(c(min(da$MR), max(da$MR))) +
@@ -85,11 +89,14 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", co
                            col = NA) +
       ggplot2::scale_color_manual("",
                                     labels = c("Euthermy", "Torpor"),
-                                    values =  c(col_eut, col_torp))
+                                    values =  c(col_eut, col_torp)) +
+      scale_y_continuous(name = paste(ylab)) +
+      scale_x_continuous(name = paste(xlab))
     if (pdf == TRUE){
-      ggsave(filename = "plot.pdf", width = 7, units = "cm")
+      ggplot2::ggsave(plot, filename = "plot.pdf", width = 7, units = "cm")
     }
 
+    return(plot)
   } else {
     X <-  pred[pred$group == "Norm", "Ta"]
     Ymeant <- pred[pred$group == "Torp", "pred"]
@@ -98,9 +105,14 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", co
     Ymeann <- pred[pred$group == "Norm", "pred"]
     Y975n <- pred[pred$group == "Norm", "upr_95"]
     Y025n <- pred[pred$group == "Norm", "lwr_95"]
-    ylab <- "MR"
+    ylab <- paste(ylab)
+    xlab <- paste(xlab)
+
     ## plot
-    graphics::plot(Y~Ta,type="n",frame=F, xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),ylab=ylab)
+    if(pdf == TRUE){
+      pdf("plot.pdf")
+    }
+    graphics::plot(Y~Ta,type="n",frame=F, xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),ylab=ylab, xlab = xlab)
     graphics::points(Y[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G<1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col= col_eut,pch=19)
     graphics::points(Y[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))]~Ta[out$mean$G>=1.5&(out$Rhat$G<1.1|is.na(out$Rhat$G))],col= col_torp,pch=19)
     graphics::points(Y[out$mean$G>=1.5&out$Rhat$G>=1.1]~Ta[out$mean$G>=1.5&out$Rhat$G>=1.1],col= col_torp,pch=19)
@@ -132,6 +144,10 @@ fit_and_plot2 <- function(mod = NULL, plot_type = "ggplot",col_torp = "grey", co
       graphics::plot(Y025n~X,xlim=c(Tlimlo, Tlimup),ylim=c(MRlo, MRup),type="l",lty=2,xaxt="n",frame=F,yaxt="n",ylab="",xlab="",col=col_eut)}
 
     graphics::legend("topright",c("Euthermy","Torpor"),pch=19, col=c(col_eut,col_torp),bty="n")
+    if(pdf == TRUE){
+      dev.off()
+    }
+
   }
 }
 
