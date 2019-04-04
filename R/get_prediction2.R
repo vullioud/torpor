3c2efff6269829bc9cb91c38355f16e5f2c87811
vullioud <colino.vullioud@gmail.com>
2019-04-04 10:55:27 +0200

    add new function get_classification() and goes back to old name

--- R/get_prediction2.R
+++ R/get_prediction2.R
@@ -1,9 +1,9 @@
-#' Get Prediction2
+#' Get Prediction
 #'
 #'The function provides the MR estimates and 95% credible interval boundaries
 #'at a defined Ta in normothermic and/or torpid stage.
-#'@name get_prediction2
-#'@aliases get_prediction2
+#'@name get_prediction
+#'@aliases get_prediction
 #'@param mod a fitted model from fit_torpor
 #'@param Ta a vector of temperatur for which the prediction should be made
 #'@return a data frame with predicted values
@@ -11,15 +11,14 @@
 #'@examples
 #'\dontrun{
 #'data(test_data2)
-#'test_mod <- fit_torpor2(MR = test_data2[,2],
+#'test_mod <- fit_torpor(MR = test_data2[,2],
 #'Ta = test_data2[, 1],
-#'BMR = 98,
+#'BMR = 1.49,
 #'TLC = 28.88,
-#'mod = NULL,
 #'fitting_options = list(nc = 1))
-#'get_prediction2(mod = test_mod, Ta = 1:20)
+#'get_prediction(mod = test_mod, Ta = 1:20)
 #'}
-get_prediction2 <- function(mod, Ta){
+get_prediction <- function(mod, Ta){
 
 
   # retrieved the posterior
@@ -111,3 +110,50 @@ funnorm2 <- function(x, inte, betat, Ym) {
   return(out)
 }
 
+################################################################################
+#' Get classification
+#'
+#'The function returns a functions with the classification (torpor or euthermy)
+#'for the observed data. It also provides the mean predicted values of MR at the
+#'given Ta.
+#'@name get_classification
+#'@aliases get_classification
+#'@param mod a fitted model from fit_torpor
+#'@return a data frame with classification and predicted MR
+#'@export
+#'@examples
+#'\dontrun{
+#'data(test_data2)
+#'test_mod <- fit_torpor(MR = test_data2[,2],
+#'Ta = test_data2[, 1],
+#'BMR = 1.49,
+#'TLC = 28.88,
+#'fitting_options = list(nc = 1))
+#'get_classification(mod = test_mod)
+#'}
+
+get_classification <- function(mod){
+x <- data.frame(measured_MR = (mod$data$Y)*mod$data$Ym[1], measured_Ta = mod$data$Ta, predicted_G = mod$mean$G)
+betat <- mod$sims.list$betat
+betac <-mod$sims.list$betac
+inte <-mod$sims.list$inte
+intc <-mod$sims.list$intc
+intr <-mod$sims.list$intr
+Tt <-mod$sims.list$Tt
+tlc <-mod$sims.list$tlc
+Ym <- mod$sims.list$Ym[1]
+
+
+X <- nrow(x)
+x$predicted_MR <- rep(NA, X)
+x$classification <- ifelse(x$predicted_G > 1.5, "Torpor", "Euthermy")
+
+for(i in 1:nrow(x)) {
+  if (x$predicted_G[i] > 1.5) {
+  x$predicted_MR[i] <- stats::median(funtorp2(x$measured_Ta[i], Tt, intr, intc, betat, betac, Ym))
+  } else {
+  x$predicted_MR[i] <- stats::median(funnorm2(x$measured_Ta[i], inte, betat, Ym))
+  }
+}
+return(x)
+}
