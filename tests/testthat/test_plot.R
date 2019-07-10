bc092e2dc780f0e51f69de8ca9228494cccf47b6
vullioud <colino.vullioud@gmail.com>
2019-07-10 18:07:12 +0200

    add test for plot output

---
+++ tests/testthat/test_plot.R
@@ -0,0 +1,6 @@
+test_that("tor_plot return a object of class ggplot if option plot_type = ggplot and ", {
+  mod <- tor_fit(MR = c(1,2,1,2,1,2), Ta = c(10,20,10,20,10,20), BMR = 10, TLC= 30, fitting_options = list(ni = 5000, nb = 3000, nc = 1))
+  x <- tor_plot(mod, plot_type = "ggplot")
+  expect_equal(class(x), c("gg", "ggplot"))
+})
+
