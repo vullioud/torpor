b0fbdb3caae2f84ebcfab71862dde4df38c70e2e
vullioud <colino.vullioud@gmail.com>
2019-07-10 17:23:27 +0200

    add a few test for complete_arg, tor_fun, eut_fun

--- tests/testthat/test_fit.R
+++ tests/testthat/test_fit.R
@@ -3,7 +3,6 @@ mod <- tor_fit(MR = c(1,2,1,2,1,2), Ta = c(10,20,10,20,10,20), BMR = 10, TLC= 30
 expect_equal(class(mod), "jagsUI")
 })
 
-
 test_that("tor_fit return an error if MR or Ta are not vectors", {
   expect_error(tor_fit(MR = data.frame(x = c(1,2,1,2,1,2)), Ta = c(10,20,10,20,10,20), BMR = 10, TLC= 30, fitting_options = list(ni = 5000, nb = 3000, nc = 1)))
 })
@@ -12,3 +11,13 @@ test_that("tor_fit return an error if MR or Ta are not the same length", {
   expect_error(tor_fit(MR = c(1,2,1)), Ta = c(10,20,10,20), BMR = 10, TLC= 30, fitting_options = list(ni = 5000, nb = 3000, nc = 1))
 })
 
+test_that("complete_args completes unclompleted list", {
+
+fun <- function(arg = list(a = 1, b=2, c = 3)){
+  complete_args(fun)
+    out <- arg$c[1]
+  return(out)
+}
+test <- fun(arg = list(c=5))
+expect_equal(test, 5)
+})
--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -19,7 +19,7 @@ test_that("tor_predict return an error if ta is not a vector of numeric", {
   expect_error(tor_predict(mod, arg2))
 })
 
-#################################################################################################################test_that("tor_predict return a data.frame of ", {
+
 test_that("tor_classify return a data.frame of length = length(data) and 5 column", {
   MR <-  c(1,2,1,2,1,2)
   Ta <- c(10,20,10,20,10,20)
@@ -32,4 +32,11 @@ expect_equal(ncol(x), 5)
 
 })
 
+test_that("tor_predict_fun correctly backtransform the data", {
+  expect_equal(round(tor_predict_fun(1,1,2,2,2,2,1), 3), 14.778)
+})
+
+test_that("heu_predict_fun correctly backtransform the data", {
+  expect_equal(round(eut_predict_fun(1,1,2,2), 3), 6)
+})
 
