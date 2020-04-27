45e4f6cba93ada07e00f998eb41dd1e560c7f026
vullioud <colino.vullioud@gmail.com>
2020-04-27 21:05:09 +0200

    New functions. Highly experimental still

--- R/tor_fit.R
+++ R/tor_fit.R
@@ -173,12 +173,12 @@ tor_fit <- function(MR,
 
 #'step_1
 #'
-#' This function estimate the lowest TLC and
+#' This function estimate the lowest TLC and the bmr
 #'
-#'@name complete_args
-#'@aliases complete_args
+#'@name step_1
 #'@param Y A vector of methabolic measure.
 #'@param Ta A vector of ambient temperature.
+#'@export
 #'@examples
 #'step_1(test_data$VO2, test_data$Ta)
 
@@ -208,7 +208,7 @@ step_1 <- function(MR,Ta){
   test_1 <- ifelse(length(na.omit(SEQX[whitetest > 0.05])) >= 1, min(SEQX[whitetest > 0.05], na.rm = TRUE), NA)
   test_2 <- ifelse(length(na.omit(SEQX[p < 0.01])) >= 1, SEQX[match(max(SEQX[p < 0.01],na.rm = TRUE) ,SEQX) - 1], NA)
 
-  heterosc <- ifelse(length(na.omit(c(test_1, test_2))) < 1, NA, max(test_1, test_2, na.rm = T))
+  heterosc <- ifelse(length(na.omit(c(test_1, test_2))) < 1, NA, max(test_1, test_2, na.rm = TRUE))
 
   if (is.na(heterosc)) stop("TLC and BMR not estimable, please provide them by hand")
 
@@ -216,4 +216,267 @@ step_1 <- function(MR,Ta){
 out <- c(bmr,heterosc)
 names(out) <- c("bmr", "low_tlc")
 return(out)
+}
+
+#'step_1_and_2
+#'
+#' This function estimate the lowest TLC and the bmr
+#'
+#'@name step_1_and_2
+#'@param Y A vector of methabolic measure.
+#'@param Ta A vector of ambient temperature.
+#'@inheritParams tor_fit
+#'@export
+step_1_and_2 <- function(Y, Ta, fitting_options = list(ni = 500000,
+                                                       nt = 10,
+                                                       nb = 300000,
+                                                       nc = 3)) {
+
+  out_1 <- step_1(Y, Ta) ## run first step
+
+  bmr <- out_1["bmr"] ## extract bmr
+  heterosc <- out_1["low_tlc"] ## extract low tlc
+
+  Ym <- mean(Y, na.rm = TRUE) ## keep the mean
+  Ta2 <- Ta[Ta < heterosc]
+  Y2 <- Y[Ta < heterosc]
+
+  inits <- list(
+    tauy = runif(1,0.05,1),
+    coef = runif(1,1,1.1),
+    p = runif(2),
+    betat = runif(1,-0.1,0),
+    tlc = runif(1,heterosc,max(Ta)),
+    Tt = runif(1),
+    TMR = runif(1,0,bmr*0.8/Ym),
+    intc1 = runif(1,0,bmr*0.8/Ym))
+
+  inits_hetero_list <- rep(list(inits), fitting_options[["nc"]])
+
+  params_hetero <- c("tlc","betat","inte")
+
+  win_data <- list(Y = Y2/Ym,
+                   NbObservations = length(Y2),
+                   Ta = Ta2,
+                   heterosc = heterosc,
+                   BMR = bmr/Ym,
+                   Max = max(Ta),
+                   Ym = Ym)
+
+  # MCMC settings
+
+
+  path_to_model_1 <- system.file("extdata", "hetero_1.txt",  package = "torpor")
+
+  out <- jagsUI::jags(data = win_data,
+                        inits = inits_hetero_list,
+                        parameters.to.save = params_hetero,
+                        model.file = path_to_model_1,
+                        n.chains = fitting_options[["nc"]],
+                        n.thin = fitting_options[["nt"]],
+                        n.iter = fitting_options[["ni"]],
+                        n.burnin = fitting_options[["nb"]],
+                        parallel = TRUE,
+                        verbose = FALSE,
+                        store.data = TRUE)
+
+return(out)
+}
+
+#'step_1_and_2
+#'
+#' This function estimate the lowest TLC and the bmr
+#'
+#'@name step_3
+#'@export
+#'@param Y A vector of methabolic measure.
+#'@param Ta A vector of ambient temperature.
+#'@inheritParams tor_fit
+#'@examples
+#'step_3(Ta = test_data$Ta, Y = test_data$VO2)
+step_3 <- function(Y, Ta, bmr = NULL, tlc = NULL, fitting_options = list(ni = 500000,
+                                                                         nt = 10,
+                                                                         nb = 300000,
+                                                                         nc = 3)){
+
+  .complete_args(step_3)
+
+  ## check input
+  if (length(Y) != length(Ta)) {
+    stop("Ta and MR have not the same length")
+  }
+
+  data <- cbind(Y, Ta)[!is.na(Y) & !is.na(Ta), ] ## remove Nas
+
+  Y <- data[, "Y"]
+  Ta <- data[, "Ta"]
+
+  ## run step 1 and 2 to get bmr and tlc
+
+  if (is.null(bmr) | is.null(tlc)) {
+    message("BMR and TLC are being
+            estimated from the data")
+    out_2 <- step_1_and_2(Ta = Ta, Y = Y)
+    bmr <- mean(Y[Ta > out_2$mean$tlc])
+    tlc <- out_2$mean$tlc
+  }
+
+  Ym <- mean(Y, na.rm = T)
+
+  ## initial values
+  inits_2 <- list(
+    tauy = runif(1,0.05,1),
+    coef = runif(1,1,1.1),
+    p = runif(3),
+    betat = runif(1,-0.1,0),
+    intc1 = runif(1,0, bmr*0.8/Ym),
+    Tt = runif(1),
+    TMR = runif(1,0, bmr*0.8/Ym))
+
+  inits_hetero_list_2 <- rep(list(inits_2), fitting_options[["nc"]])
+
+  params_hetero_2 <- c("tau",
+                       "G",
+                       "p",
+                       "inte",
+                       "intc",
+                       "intr",
+                       "betat",
+                       "betac",
+                       "Tt",
+                       "TMR",
+                       "tlc")
+
+  win.data_2 <- list(Y = Y/Ym,
+                     NbObservations = length(Y),
+                     Ta = Ta,
+                     BMR = bmr/Ym,
+                     tlc = tlc,
+                     Ym = Ym)
+
+  path_to_model_2 <- system.file("extdata", "hetero_2.txt",  package = "torpor")
+
+  out2 <- jagsUI::jags(data = win.data_2,
+                       inits = inits_hetero_list_2,
+                       parameters.to.save = params_hetero_2,
+                       model.file = path_to_model_2,
+                       n.chains = fitting_options[["nc"]],
+                       n.thin = fitting_options[["nt"]],
+                       n.iter = fitting_options[["ni"]],
+                       n.burnin = fitting_options[["nb"]],
+                       parallel = TRUE,
+                       verbose = FALSE,
+                       store.data = TRUE)
+
+  return(out2)
+
+}
+
+
+#'step_3_bis
+#'
+#' This function estimate the lowest TLC and the bmr
+#'
+#'@name step_3_bis
+#'@param mod A fitted model
+#'@export
+step_3_bis <- function(mod){
+
+  betat <- mod$sims.list$betat
+  betac <- mod$sims.list$betac
+  inte <- mod$sims.list$inte
+  intc <- mod$sims.list$intc
+  intr <- mod$sims.list$intr
+  Tt <- mod$sims.list$Tt
+
+  Ym <- mod$data$Ym
+  Ta <- mod$data$Ta
+  Y <- mod$data$Y
+  tlc <- mod$data$tlc
+
+  G <- mod$mean$G
+
+  G[sqrt((G - round(G))^2) > 0.496] <- 0
+  G <- round(G)
+
+  if (length(G[G == 1]) !=0 ) {
+
+    for (i in 1:length(Y <= tlc)) {
+
+      #Only proceed to this step if there are at least one torpid (G==1) value.
+      G[i] <- ifelse(Y[i] <= stats::median(tor_predict_fun(Ta[i], Tt, intr, intc, betat, betac, Ym)) & G[i] != 3, 1, G[i])
+      G[i] <- ifelse(Y[i] >=  stats::median(eut_predict_fun(Ta[i], inte, betat, Ym)) & G[i] != 3, 2 ,G[i])
+    }
+
+  }
+  return(G)
+}
+
+#'step_4
+#'
+#' This function estimate the lowest TLC and the bmr
+#'
+#'@name step_4
+#'@inheritParams step_3
+#'@export
+#'@examples
+#'test_mod <- step_4(Ta = test_data3$Ta, Y = test_data3$VO2)
+step_4 <- function(Ta, Y,  bmr = NULL, tlc = NULL, fitting_options = list(ni = 500000,
+                                                                          nt = 10,
+                                                                          nb = 300000,
+                                                                          nc = 3)) {
+
+
+  ## run step 1-3
+  out_2 <- step_3(Ta = Ta, Y = Y,  bmr = bmr, tlc = tlc, fitting_options = fitting_options)
+
+  ## manual
+  tt <- step_3_bis(out_2)
+
+  Y <- out_2$data$Y
+  Ta <- out_2$data$Ta
+  Ym <- out_2$data$Ym
+  tlc <- out_2$data$tlc
+  bmr <- out_2$data$BMR
+
+  G <- tt
+
+  if (length(Ta) != length(Y) | length(Y) != length(G)) stop("Y, Ta and G don´t have the same length")
+
+  win.data_3 <- list(Y = Y[G != 0 & G != 3] / Ym,
+                     NbObservations = length(Y[G != 0 & G != 3]),
+                     Ta = Ta[G != 0 & G != 3],
+                     BMR = bmr / Ym,
+                     tlc = tlc,
+                     G = G[G != 0 & G != 3],
+                     Ym = Ym)
+
+
+  path_to_model_3 <- system.file("extdata", "hetero_3.txt",  package = "torpor")
+
+  inits_3<- list(
+    tauy = runif(1,0.05,1),
+    coef = runif(1,1,1.1),
+    p = runif(3),
+    betat = runif(1,-0.1,0),
+    intc1 = runif(1,0, bmr*0.8/Ym),
+    Tt = runif(1),
+    TMR = runif(1,0, bmr*0.8/Ym))
+
+  inits_hetero_list_3 <- rep(list(inits_3), fitting_options[["nc"]])
+
+  out3 <- jagsUI::jags(data = win.data_3,
+                       inits =  inits_hetero_list_3,
+                       parameters.to.save = out_2$parameters,
+                       model.file = path_to_model_3,
+                       n.chains = fitting_options[["nc"]],
+                       n.thin = fitting_options[["nt"]],
+                       n.iter = fitting_options[["ni"]],
+                       n.burnin = fitting_options[["nb"]],
+                       parallel = TRUE,
+                       verbose = FALSE,
+                       store.data = TRUE)
+
+
+  return(out3)
 }
