9effd8a4275a0df4c5a9d5756c84fa16030095c2
colin <you@example.com>
2020-10-09 15:38:35 +0200

    Write test

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -1,6 +1,6 @@
-# test_that("tor_plot return a object of class ggplot if option plot_type = ggplot and ", {
-#   mod <- tor_fit(MR = c(1,2,1,2,1,2), Ta = c(10,20,10,20,10,20), BMR = 10, TLC= 30, fitting_options = list(ni = 5000, nb = 3000, nc = 1))
-#   x <- tor_plot(mod, plot_type = "ggplot")
-#   expect_equal(class(x), c("gg", "ggplot"))
-# })
-#
+ test_that("tor_plot return a object of class ggplot if option plot_type = ggplot", {
+  expect_warning(mod <- tor_fit(M = test_data$VO2, Ta = test_data$Ta, fitting_options = list(ni = 5000, nb = 3000, nc = 1)))
+  expect_warning(x <- tor_plot(mod, plot_type = "ggplot"))
+   expect_equal(class(x), c("gg", "ggplot"))
+})
+
