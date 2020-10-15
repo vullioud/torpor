7d225812d07393ddd21fe8a925aec8cf916562f6
colin <colino.vullioud@gmail.com>
2020-10-15 20:38:36 +0200

    Move method to zzz

--- R/tor_fit.R
+++ R/tor_fit.R
@@ -496,26 +496,3 @@ tor_fit <- function(Ta, M,
   return(invisible(out_assignation))
 
 }
-
-### coommon method to ease the usage
-#######
-#' @export
-#' @method print tor_obj
-print.tor_obj <- function(x, ...) {
-  print(tor_summarise(x, ...))
-}
-
-#######
-#' @export
-#' @method summary tor_obj
-summary.tor_obj <- function(object, ...) {
-  tor_summarise(object)
-}
-
-#######
-#' @export
-#' @method plot tor_obj
-plot.tor_obj <- function(x, ...) {
-  tor_plot(tor_obj = x,...)
-}
-class(plot.tor_obj)
---
+++ R/zzz.R
@@ -0,0 +1,39 @@
+### coommon method to ease the usage
+#######
+#' @export
+#' @method print tor_obj
+print.tor_obj <- function(x, ...) {
+  print(tor_summarise(x, ...))
+}
+
+#######
+#' @export
+#' @method summary tor_obj
+summary.tor_obj <- function(object, ...) {
+  tor_summarise(object)
+}
+
+#######
+#' @export
+#' @method plot tor_obj
+plot.tor_obj <- function(x, ...) {
+  tor_plot(tor_obj = x,...)
+}
+
+#######
+
+#' Display welcome message when loading the package
+#'
+#' It displays a message when the package is being loaded.
+#' @param  libname a character string giving the library directory where the package defining the namespace was found.
+#' @param pkgname a character string giving the name of the package.
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "\n",
+    "Torpor version: ", utils::packageDescription("torpor")$Version, " is loaded",
+    "\n",
+    "Run: vignette('example') or browseVignettes('torpor') for an example",
+    "\n",
+    "Find more information about the model in the companion article"
+  )
+}
