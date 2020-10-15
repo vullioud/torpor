c6fbb430cadf95cd70bb2ea9884b37d5059bdfe2
colin <colino.vullioud@gmail.com>
2020-10-15 16:23:52 +0200

    test for base-r

--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -4,3 +4,9 @@
    expect_equal(class(x), c("gg", "ggplot"))
 })
 
+ test_that("tor_plot return a object of class NULL if option plot_type = base", {
+   expect_warning(mod <- tor_fit(M = test_data$VO2, Ta = test_data$Ta, fitting_options = list(ni = 5000, nb = 3000, nc = 1)))
+   expect_warning(x <- tor_plot(mod))
+   expect_equal(class(x), c("NULL"))
+ })
+
