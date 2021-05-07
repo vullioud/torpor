fa4a76006a5572e9840e103226905f39dc852f14
colin <colino.vullioud@gmail.com>
2021-05-07 19:36:28 +0200

    Adapted ppo test dim.

--- tests/testthat/test_summary.R
+++ tests/testthat/test_summary.R
@@ -9,12 +9,12 @@
    expect_equal(class(test[[2]]), "data.frame")
  })
 
- test_that("tor_overlap return a data.frame of 3 rows and 2 columns", {
+ test_that("tor_ppo return a data.frame of 2 rows and 2 columns", {
    set.seed(1)
    expect_warning(mod <- tor_fit(M = test_data$VO2, Ta = test_data$Ta, fitting_options = list(ni = 5000, nb = 3000, nc = 1)))
    set.seed(1)
    expect_warning(test <- tor_ppo(tor_obj = mod))
-   expect_equal(dim(test), c(5,2))
+   expect_equal(dim(test), c(2,2))
    expect_equal(class(test)[1], "data.frame")
  })
 
@@ -26,7 +26,7 @@
    expect_error(tor_summarise(mod))
 })
 #
-test_that("tor_overlap and tor_summarise return a warning if tlc_overlap, Tt_overlap and Betat_overla >= 0.3)", {
+test_that("tor_ppo and tor_summarise return a warning if tlc_overlap, Tt_overlap and Betat_overla >= 0.3)", {
    set.seed(1)
    expect_warning(mod <- tor_fit(M = test_data$VO2, Ta = test_data$Ta, fitting_options = list(ni = 5000, nb = 3000, nc = 1)))
    expect_warning(tor_ppo(mod))
